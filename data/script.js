3d0263ed0c09756c49494d087a423c1995a804ea
Juan Damian Lucero <juandamian18@hotmail.com>
2024-09-30 02:00:53 -0300

    Cambio en la interfaz de index.htm

--- data/script.js
+++ data/script.js
@@ -1,73 +1,69 @@
 (function($) {
-
-    $.fn.menumaker = function(options) {
-        
-        var cssmenu = $(this), settings = $.extend({
-          title: "Menu",
+  $.fn.menumaker = function(options) {
+      var cssmenu = $(this), settings = $.extend({
+          title: "Menu",  // Esta línea ya no es necesaria, se puede eliminar
           format: "dropdown",
           sticky: false
-        }, options);
-  
-        return this.each(function() {
-          cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-          $(this).find("#menu-button").on('click', function(){
-            $(this).toggleClass('menu-opened');
-            var mainmenu = $(this).next('ul');
-            if (mainmenu.hasClass('open')) { 
-              mainmenu.hide().removeClass('open');
-            }
-            else {
-              mainmenu.show().addClass('open');
-              if (settings.format === "dropdown") {
-                mainmenu.find('ul').show();
-              }
-            }
-          });
-  
-          cssmenu.find('li ul').parent().addClass('has-sub');
-  
-          multiTg = function() {
-            cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-            cssmenu.find('.submenu-button').on('click', function() {
-              $(this).toggleClass('submenu-opened');
-              if ($(this).siblings('ul').hasClass('open')) {
-                $(this).siblings('ul').removeClass('open').hide();
+      }, options);
+
+      return this.each(function() {
+          // Aquí se inserta el logo en lugar del título
+          cssmenu.prepend('<div id="menu-button"><img src="logo.svg" alt="Logo" style="height: 30px;margin-top: 5px;margin-bottom: 5px;"></div>');
+          $(this).find("#menu-button").on('click', function() {
+              $(this).toggleClass('menu-opened');
+              var mainmenu = $(this).next('ul');
+              if (mainmenu.hasClass('open')) { 
+                  mainmenu.hide().removeClass('open');
               }
               else {
-                $(this).siblings('ul').addClass('open').show();
+                  mainmenu.show().addClass('open');
+                  if (settings.format === "dropdown") {
+                      mainmenu.find('ul').show();
+                  }
               }
-            });
+          });
+
+          cssmenu.find('li ul').parent().addClass('has-sub');
+
+          multiTg = function() {
+              cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+              cssmenu.find('.submenu-button').on('click', function() {
+                  $(this).toggleClass('submenu-opened');
+                  if ($(this).siblings('ul').hasClass('open')) {
+                      $(this).siblings('ul').removeClass('open').hide();
+                  }
+                  else {
+                      $(this).siblings('ul').addClass('open').show();
+                  }
+              });
           };
-  
+
           if (settings.format === 'multitoggle') multiTg();
           else cssmenu.addClass('dropdown');
-  
+
           if (settings.sticky === true) cssmenu.css('position', 'fixed');
-  
+
           resizeFix = function() {
-            if ($( window ).width() > 768) {
-              cssmenu.find('ul').show();
-            }
-  
-            if ($(window).width() <= 768) {
-              cssmenu.find('ul').hide().removeClass('open');
-            }
+              if ($( window ).width() > 768) {
+                  cssmenu.find('ul').show();
+              }
+
+              if ($(window).width() <= 768) {
+                  cssmenu.find('ul').hide().removeClass('open');
+              }
           };
           resizeFix();
           return $(window).on('resize', resizeFix);
-  
-        });
-    };
-  })(jQuery);
-  
-  (function($){
+      });
+  };
+})(jQuery);
+
+(function($){
   $(document).ready(function(){
-  
-  $("#cssmenu").menumaker({
-     title: "powermeter",
-     format: "multitoggle"
+      $("#cssmenu").menumaker({
+          // Puedes quitar el título aquí o dejarlo vacío
+          title: "",  // Este título ya no se utiliza
+          format: "multitoggle"
+      });
   });
-  
-  });
-  })(jQuery);
-  
+})(jQuery);
