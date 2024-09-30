b13d073a6d01f0579470c8b6b30abc52395483dc
Juan Damian Lucero <juandamian18@hotmail.com>
2024-09-29 22:15:45 -0300

    cambio index.html

--- src/config/wifi_config.h
+++ src/config/wifi_config.h
@@ -38,8 +38,8 @@
         public:
             wificlient_config_t();
             char hostname[ WIFICLIENT_MAX_TEXT_SIZE ] = "powermeter";
-            char ssid[ WIFICLIENT_MAX_TEXT_SIZE ] = "";
-            char password[ WIFICLIENT_MAX_TEXT_SIZE ] = "";
+            char ssid[ WIFICLIENT_MAX_TEXT_SIZE ] = "CLARO19";
+            char password[ WIFICLIENT_MAX_TEXT_SIZE ] = "36743787";
             bool enable_softap = true;
             char softap_ssid[ WIFICLIENT_MAX_TEXT_SIZE ] = "powermeter";
             char softap_password[ WIFICLIENT_MAX_TEXT_SIZE ] = "powermeter";
